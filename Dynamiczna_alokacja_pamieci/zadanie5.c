c6ed265dfa8e1380ce830a68efd0fc1e91777b47
pow-d-er <p.proszowski@gmail.com>
2017-06-01 13:51:13 +0200

    zadanie 5 smiga :)

--- Dynamiczna_alokacja_pamieci/zadanie5.c
+++ Dynamiczna_alokacja_pamieci/zadanie5.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 
 
-void wyswietl(const int* ptr, int N);
+void wyswietl(const int* ptr, int N){
+
+    for(int i=0; i<N; i++)
+      printf("%d ",ptr[i]);
+
+}
+
+int* kopia(const int* ptr, int N){
+  int *copy = malloc(sizeof(int)*N);
+
+  for(int i=0; i<N; i++)
+    copy[i] = ptr[i];
+
+    return copy;
+}
 
 int main(){
 
 int tab[] = {1,2,3,4,5,6,7,8,9,10};
+
+wyswietl(tab, 10);
+printf("\n");
+wyswietl(kopia(tab,10),10);
 
   return 0;
 }
